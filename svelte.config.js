f81e8e2d3fc7947b82bce44561de49b1d37863e1
Szilvasi Peter <peti.szilvasi95@gmail.com>
2024-02-19 16:49:23 +0100

    Disable kit.paths config

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,14 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
       fallback: 'index.html',
       precompress: false,
       strict: true
     }),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
+    // paths: {
+    //   base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    // }
   }
 };

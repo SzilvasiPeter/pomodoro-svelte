191534647cf73cdf16279dde9d97a16ddecd7195
Szilvasi Peter <peti.szilvasi95@gmail.com>
2024-02-19 15:10:44 +0100

    Add kit.paths.base configuration

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,9 @@ export default {
       fallback: '404.html',
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
   }
 };

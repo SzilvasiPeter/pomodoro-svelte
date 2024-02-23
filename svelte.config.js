bd68215ad3b93468f991af2f00d00315f25ee7a9
Szilvasi Peter <peti.szilvasi95@gmail.com>
2024-02-23 10:26:08 +0100

    Remove the default values for the adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
+      fallback: 'index.html'
     }),
     // The `paths` configuration is necessary for the GitHub page deployment
     paths: {

77b3c220963ca7624dc058b86190f2e54ebfbb2b
Szilvasi Peter <peti.szilvasi95@gmail.com>
2024-03-14 17:42:04 +0100

    Add a settings dialog as modal

--- src/routes/Pomodoro.svelte
+++ src/routes/Pomodoro.svelte
@@ -5,12 +5,19 @@
 
   dayjs.extend(duration);
 
+  let workTime = 25;
+  let shortBreakTime = 5;
+  let longBreakTime = 10;
+  
+  let dialog;
   let interval;
   let isActive = false;
-  let minutes = 25;
-  $: pomodoro = dayjs.duration(minutes, 'minutes');
+
+  let pomodoro = dayjs.duration(workTime, 'minutes');
 
   onMount(() => {
+    dialog = document.getElementById('confirmation-dialog');
+    loadSettings();
     resetTimer();
   });
 
@@ -31,7 +38,7 @@
   function resetTimer() {
     clearInterval(interval);
     isActive = false;
-    pomodoro = dayjs.duration(minutes, 'minutes');
+    pomodoro = dayjs.duration(workTime, 'minutes');
   }
 
   function toggleTimer() {
@@ -43,14 +50,56 @@
       isActive = true;
     }
   }
+
+  function saveSettings() {
+    pomodoro = dayjs.duration(workTime, 'minutes');
+    localStorage.setItem('pomodoroSettings', JSON.stringify({ workTime, shortBreakTime, longBreakTime }));
+    dialog.close();
+  }
+
+  function loadSettings() {
+    const savedSettings = localStorage.getItem('pomodoroSettings');
+    if (savedSettings) {
+      const { workTime: savedWorkTime, shortBreakTime: savedShortBreakTime, longBreakTime: savedLongBreakTime } = JSON.parse(savedSettings);
+      workTime = savedWorkTime;
+      shortBreakTime = savedShortBreakTime;
+      longBreakTime = savedLongBreakTime;
+    }
+  }
+
+  function closeSettings() {
+    dialog.close();
+  }
+
+  const showDialogClick = () => {
+    dialog['showModal']();
+	};
 </script>
 
+<dialog id="confirmation-dialog">
+  <form>
+    <h4>Time</h4>
+    <hr/>
+    <label for="workTime">Pomodoro: {workTime} minutes</label>
+    <input type="range" bind:value={workTime} min="1" max="120" step="1" />
+    <label for="shortBreakTime">Short Break: {shortBreakTime} minutes</label>
+    <input type="range" bind:value={shortBreakTime} min="1" max="120" step="1" />
+    <label for="longBreakTime">Long Break: {longBreakTime} minutes</label>
+    <input type="range" bind:value={longBreakTime} min="1" max="120" step="1" />
+    <br/>
+    <button type="button" on:click={saveSettings}>Save</button>
+    <button type="button" on:click={closeSettings}>Close</button>
+  </form>
+</dialog>
+
+<header>
+  <button on:click={showDialogClick}>Settings</button>
+</header>
+
 <div>
-  <h1>Pomodoro</h1>
-  <input bind:value={minutes} disabled={isActive} type="number" />
+  <p>{pomodoro.format('HH:mm:ss')}</p>
   <button on:click={toggleTimer}>{isActive ? 'Pause' : 'Start'}</button>
   {#if isActive}
     <button on:click={resetTimer}>Finish</button>
   {/if}
-  <p>{pomodoro.format('HH:mm:ss')}</p>
 </div>
